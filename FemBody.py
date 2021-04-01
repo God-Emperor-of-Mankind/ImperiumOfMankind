ab610ef8ec10aa0a1034cc9275e9aa501bcda150
Emperor <mihanen1747@gmail.com>
2021-04-01 17:51:44 +0300

    FEM main file

--- FemBody.py
+++ FemBody.py
@@ -20,6 +20,7 @@ K = border_condition(K)
 print("Матрица жескости после введения ГУ" + "\n" + "K:", K)
 
 # Вектор внешних узловых усилий:
+
 f = [0, F, 0]
 
 print("Вектор внешних узловых усилий:" + "\n" + "f:", f)
@@ -27,6 +28,24 @@ print("Вектор внешних узловых усилий:" + "\n" + "f:", 
 # Неизвестное перемещение во втром узле
 
 from unkown_second_node_displacement import usnd
-u = usnd(f, K)
+u1, u2, u3, u = usnd(f, K)
 print("Неизвестное перемещение во втром узле:" + "\n" + "u:", u)
 
+#перемещения первого элемента
+
+import first_node_displacement
+first_node_displacement.fnd(u1, u2)
+
+#перемещения второго элемента
+
+import second_node_displacement
+second_node_displacement.snd(u2, u3)
+
+# Внутренние усилия в стержне:
+# N = EFu'
+
+import first_node_force
+first_node_force.fnf(u1, u2, EF1)
+
+import second_node_force
+second_node_force.snf(u2, u3, EF2)
